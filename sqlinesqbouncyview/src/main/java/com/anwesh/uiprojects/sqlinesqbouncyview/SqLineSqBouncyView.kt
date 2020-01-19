b0e7cacccd2ef96c0e1fbd0b37db3f311ff2fc08
Anwesh43 <anweshthecool0@gmail.com>
2020-01-19 18:11:36 +0530

    adding Canvas extension method

--- sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
+++ sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
@@ -17,13 +17,57 @@ import android.graphics.RectF
 val nodes : Int = 5
 val squares : Int = 2
 val scGap : Float = 0.02f / (2 * squares - 1)
-val sizeFactor : Float = 2.9f
+val sizeFactor : Float = 1.3f
 val strokeFactor : Float = 90f
 val grayBack : Int = Color.parseColor("#9E9E9E")
 val fillBack : Int = Color.parseColor("#3F51B5")
-val foreColor : Int = Color.parseColor("#BDBDBD")
+val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSqLine(i : Int, scale : Float, h : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify().divideScale(i, squares)
+    var lines : Int = 0
+    val sc1 : Float = sf.divideScale(0, lines + 1)
+    var sc2 : Float = 0f
+    if (i != squares - 1) {
+        lines = 1
+        sc2 = sf.divideScale(1, lines + 1)
+    }
+    val hGap : Float = (h) / (squares + 2)
+    val y : Float = h - hGap / 2 - hGap * i
+    val yBar : Float = -size * sc1
+    val yLine : Float = -hGap * sc2
+    val yLF : Float = -size / 2
+    save()
+    translate(0f, y)
+    paint.color = grayBack
+    drawRect(RectF(-size / 2, -size / 2, size / 2, size / 2), paint)
+    drawLine(0f, yLF, 0f, yLF - hGap * lines, paint)
+    paint.color = fillBack
+    drawRect(RectF(-size / 2, size / 2, size / 2, size / 2 + yBar), paint)
+    drawLine(0f, yLF + yLine, 0f, yLF -hGap, paint)
+    restore()
+}
+
+fun Canvas.drawSqLineSq(scale : Float, h : Float, size : Float, paint : Paint) {
+    for (j in 0..(squares - 1)) {
+        drawSqLine(j, scale, h, size, paint)
+    }
+}
+
+fun Canvas.drawSLSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(gap * (i + 1), 0f)
+    drawSqLineSq(scale, h, size, paint)
+    restore()
+}
