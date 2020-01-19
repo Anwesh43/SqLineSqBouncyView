1eb7d836539c2ed1472772b367adecaed263b528
Anwesh43 <anweshthecool0@gmail.com>
2020-01-19 18:16:31 +0530

    adding Canvas extension method

--- sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
+++ sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
@@ -71,3 +71,21 @@ fun Canvas.drawSLSNode(i : Int, scale : Float, paint : Paint) {
     drawSqLineSq(scale, h, size, paint)
     restore()
 }
+
+class SqLineSqBouncyView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
