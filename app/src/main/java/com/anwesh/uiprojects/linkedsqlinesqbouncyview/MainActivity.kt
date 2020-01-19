8a3b619b39a561dfd87610ac733f9bc8e3064b66
Anwesh43 <anweshthecool0@gmail.com>
2020-01-19 18:48:03 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsqlinesqbouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsqlinesqbouncyview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedsqlinesqbouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.sqlinesqbouncyview.SqLineSqBouncyView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SqLineSqBouncyView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
