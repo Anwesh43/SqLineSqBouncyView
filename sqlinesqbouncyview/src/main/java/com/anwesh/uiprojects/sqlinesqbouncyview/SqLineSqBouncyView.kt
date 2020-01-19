384254abbdf92383b4f7a814857c553eb40c501a
Anwesh43 <anweshthecool0@gmail.com>
2020-01-19 18:41:23 +0530

    adding SqLineSq class

--- sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
+++ sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
@@ -179,4 +179,28 @@ class SqLineSqBouncyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class SqLineSq(var i : Int) {
+
+        private val root : SLSNode = SLSNode(0)
+        private var curr : SLSNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
