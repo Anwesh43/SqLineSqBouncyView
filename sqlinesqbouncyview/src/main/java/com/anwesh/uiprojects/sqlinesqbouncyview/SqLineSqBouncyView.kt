a7de91605d678481cb55286af8a90b5800ba7388
Anwesh43 <anweshthecool0@gmail.com>
2020-01-19 19:19:01 +0530

    adding fixes

--- sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
+++ sqlinesqbouncyview/src/main/java/com/anwesh/uiprojects/sqlinesqbouncyview/SqLineSqBouncyView.kt
@@ -16,13 +16,13 @@ import android.graphics.RectF
 
 val nodes : Int = 5
 val squares : Int = 2
-val scGap : Float = 0.02f / (2 * squares - 1)
+val scGap : Float = 0.01f / (2 * squares - 1)
 val sizeFactor : Float = 1.3f
 val strokeFactor : Float = 90f
 val grayBack : Int = Color.parseColor("#9E9E9E")
 val fillBack : Int = Color.parseColor("#3F51B5")
 val backColor : Int = Color.parseColor("#BDBDBD")
-val delay : Long = 20
+val delay : Long = 20L / squares
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
